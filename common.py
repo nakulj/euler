106c5bd603cfe945944953f1af60dd2df560ce0c
nakulj <nakuljos@usc.edu>
2015-05-10 22:37:27 -0700

    Make it a generator to be fancier

--- common.py
+++ common.py
@@ -1,13 +1,12 @@
 def sieve(n):
 	if n<2:
-		return []
+		return
 	
 	is_prime = [True] * (n)
 	
 	for i in range(2,n):
 		if not is_prime[i]:
 			continue
+		yield i
 		for j in range(2*i, n, i):
 			is_prime[j] = False
-	
-	return [i for i in range(2,n) if is_prime[i]]
